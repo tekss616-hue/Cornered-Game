bc84fb77be9f038c2294540b9d60aff0d2069bd2
tekss616-hue <tekss616@gmail.com>
2026-09-09 10:16:46 +0300

    Connect Android app to new Cornered server

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.cornered.game"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
 
         buildConfigField("String", "FIREBASE_API_KEY", "\"${System.getenv("CORNERED_FIREBASE_API_KEY") ?: ""}\"")
         buildConfigField("String", "FIREBASE_APP_ID", "\"1:228318611339:android:0dc033187e6921e974518e\"")
@@ -19,7 +19,7 @@ android {
         buildConfigField("String", "FIREBASE_DATABASE_URL", "\"https://veilmark-d2480-default-rtdb.firebaseio.com\"")
         buildConfigField("String", "FIREBASE_STORAGE_BUCKET", "\"veilmark-d2480.firebasestorage.app\"")
         buildConfigField("String", "FIREBASE_WEB_CLIENT_ID", "\"228318611339-aehctm7ggfreblqfpddd8efglgh87rtt.apps.googleusercontent.com\"")
-        buildConfigField("String", "SERVER_BASE_URL", "\"${System.getenv("CORNERED_SERVER_BASE_URL") ?: ""}\"")
+        buildConfigField("String", "SERVER_BASE_URL", "\"https://cornered-server.onrender.com\"")
     }
 
     buildFeatures {
