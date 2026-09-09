4c54e0c64481e3103e5044129e57f13d4ca1c02c
tekss616-hue <tekss616@gmail.com>
2026-09-09 11:30:44 +0300

    Build Cornered Android 0.1.8

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.cornered.game"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.1.7"
+        versionCode = 9
+        versionName = "0.1.8"
 
         buildConfigField("String", "FIREBASE_API_KEY", javaString(firebaseApiKey))
         buildConfigField("String", "FIREBASE_APP_ID", javaString("1:228318611339:android:0dc033187e6921e974518e"))
