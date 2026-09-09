42a8dec110e8a7b749dfa1c27917762a9460153b
tekss616-hue <tekss616@gmail.com>
2026-09-09 09:33:05 +0300

    Use stable Android API 36 for build

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.cornered.game"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.cornered.game"
         minSdk = 24
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
     }
